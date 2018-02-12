07b345cfd8a63c168cb1bbcee500ed3e0d76198f
bharathreddy <bharathreddy411@gmail.com>
2018-02-12 17:32:54 +0530

    Added spinner

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/hello', function(req, res) {
+  res.send('Hello World!');
+});
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+    res.sendFile(__dirname, '/public/index.html');
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
